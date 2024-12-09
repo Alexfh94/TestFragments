29f3072a664df7435665c876ddd0ceebbbc4eea2
Alex <alexfh94@gmail.com>
2024-12-09 20:02:50 +0100

    Initial commit

---
+++ app/src/main/java/com/example/testfragments/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.testfragments;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button button = findViewById(R.id.button);
+        EditText editText = findViewById(R.id.editTextText);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View view) {
+
+
+                BlankFragment fragment = new BlankFragment();
+                Bundle bundle = new Bundle();
+                bundle.putString("texto", editText.getText().toString());
+                fragment.setArguments(bundle);
+
+                // Administrar el fragmento con el FragmentManager
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                fragmentTransaction.replace(R.id.fragmentContainerView, fragment);
+                fragmentTransaction.commit();
+
+
+            }
+        });
+
+    }
+
+}
